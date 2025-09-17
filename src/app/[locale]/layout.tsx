4f7b2e4004543e01e87ae44ae60b459269d8eae7
cuong03dev <cuong.hoang.kim@bunbu.vn>
2025-09-17 15:33:48 +0700

    Config i18n for login page

---
+++ src/app/[locale]/layout.tsx
@@ -0,0 +1,16 @@
+import { NextIntlClientProvider, hasLocale } from 'next-intl'
+import { notFound } from 'next/navigation'
+import { routing } from '@/i18n/routing'
+
+type Props = {
+  children: React.ReactNode
+  params: { locale: string }
+}
+
+export default async function LocaleLayout({ children, params }: Props) {
+  const { locale } = await params
+  if (!hasLocale(routing.locales, locale)) {
+    notFound()
+  }
+  return <NextIntlClientProvider>{children}</NextIntlClientProvider>
+}
