3a9e6f407da6ff4bc83d1e2342876a98f0e24912
cuong03dev <cuong.hoang.kim@bunbu.vn>
2025-09-17 09:22:22 +0700

    Config eslint, prettier

---
+++ eslint.config.mjs
@@ -0,0 +1,14 @@
+// eslint.config.mjs
+import { FlatCompat } from '@eslint/eslintrc'
+
+const compat = new FlatCompat({
+  baseDirectory: import.meta.dirname,
+})
+
+const eslintConfig = [
+  ...compat.config({
+    extends: ['next/core-web-vitals', 'next/typescript', 'prettier'],
+  }),
+]
+
+export default eslintConfig
