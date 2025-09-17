7cb9ecf76b6b58801a0263115bd5d96c95af4fcc
cuong03dev <cuong.hoang.kim@bunbu.vn>
2025-09-17 11:15:41 +0700

    Split component

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,49 +1,51 @@
+import FormField from '@/components/ui-parts/FormField'
+import Button from '@/components/ui/Button'
+
 export default function LoginPage() {
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
-        <div className="sm:mx-auto sm:w-full sm:max-w-md">
-            <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-                Sign in to your account
-            </h2>
-            <p className="mt-2 text-center text-sm text-gray-600 max-w">
-                Or <br></br>
-                <a href="#" className="font-medium text-blue-600 hover:text-blue-500">
-                    Create an account
-                </a>
-            </p>
+      <div className="sm:mx-auto sm:w-full sm:max-w-md">
+        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+          Sign in to your account
+        </h2>
+        <p className="mt-2 text-center text-sm text-gray-600 max-w">
+          Or <br></br>
+          <a href="#" className="font-medium text-blue-600 hover:text-blue-500">
+            Create an account
+          </a>
+        </p>
+      </div>
+      <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+        <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+          <form className="space-y-6" action="#" method="POST">
+            <FormField
+              label="Email address"
+              id="email"
+              name="email"
+              type="email"
+              autoComplete="email"
+              placeholder="Enter your email address"
+              labelClass="block text-sm font-medium text-gray-700"
+              inputClass="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+              required
+            />
+            <FormField
+              label="Password"
+              id="password"
+              name="password"
+              type="password"
+              autoComplete="current-password"
+              placeholder="Enter your password"
+              labelClass="block text-sm font-medium text-gray-700"
+              inputClass="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+              required
+            />
+            <Button className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+              Sign In
+            </Button>
+          </form>
         </div>
-        <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-            <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-                <form className="space-y-6" action="#" method="POST">
-                    <div>
-                        <label htmlFor="email" className="block text-sm font-medium text-gray-700">
-                            Email address
-                        </label>
-                        <div className="mt-1">
-                            <input id="email" name="email" type="email" autoComplete="email" required
-                                className="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                                placeholder="Enter your email address"></input>
-                        </div>
-                    </div>
-                    <div>
-                        <label htmlFor="password" className="block text-sm font-medium text-gray-700">
-                            Password
-                        </label>
-                        <div className="mt-1">
-                            <input id="password" name="password" type="password" autoComplete="current-password" required
-                                className="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
-                                placeholder="Enter your password"></input>
-                        </div>
-                    </div>
-                    <div>
-                        <button type="submit"
-                            className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                            Sign in
-                        </button>
-                    </div>
-                </form>
-            </div>
-        </div>
+      </div>
     </div>
-  );
+  )
 }
---
+++ src/components/ui-parts/FormField.tsx
@@ -0,0 +1,43 @@
+import Input from '../ui/Input'
+import Label from '../ui/Label'
+
+interface FormFieldProps {
+  id: string
+  name: string
+  label: string
+  type?: string
+  placeholder?: string
+  autoComplete?: string
+  required?: boolean
+  inputClass?: string
+  labelClass?: string
+}
+
+export default function FormField({
+  id,
+  name,
+  label,
+  type,
+  placeholder,
+  autoComplete,
+  inputClass,
+  labelClass,
+  required,
+}: FormFieldProps) {
+  return (
+    <div>
+      <Label className={labelClass} htmlFor={name} text={label}></Label>
+      <div className="mt-1">
+        <Input
+          id={id}
+          name={name}
+          type={type}
+          autoComplete={autoComplete}
+          placeholder={placeholder}
+          className={inputClass}
+          required={required}
+        />
+      </div>
+    </div>
+  )
+}
