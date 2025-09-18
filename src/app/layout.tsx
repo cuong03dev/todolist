60c351692598dd4cddbab920cb73c22d1095ec54
cuong03dev <cuong.hoang.kim@bunbu.vn>
2025-09-18 08:59:02 +0700

    Revert

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,16 @@
 import type { Metadata } from 'next'
+import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
-import I18Provider from '@/provider/I18Provider'
+
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+})
+
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+})
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -13,9 +23,11 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html>
-      <body>
-        <I18Provider>{children}</I18Provider>
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
       </body>
     </html>
   )
