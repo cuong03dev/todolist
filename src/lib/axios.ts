171ba5317ea5e2753fe517c8a6375eddf15f1002
cuong03dev <cuong.hoang.kim@bunbu.vn>
2025-09-24 15:11:10 +0700

    Extract getErrorMessage function and handleTokenRefresh

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -23,68 +23,67 @@ http.interceptors.request.use(
   (error) => Promise.reject(error),
 )
 
+const handleTokenRefresh = async (error) => {
+  const refreshToken = Cookies.get('refreshToken')
+  if (refreshToken) {
+    try {
+      const res = await axios.post(
+        `${baseUrl}user/reissue`,
+        { refreshToken },
+        { withCredentials: true },
+      )
+
+      const newAccessToken = res?.data
+      Cookies.set('accessToken', newAccessToken, { expires: 1 / 96 })
+
+      error.config.headers.Authorization = `Bearer ${newAccessToken}`
+      return http.request(error.config)
+    } catch {
+      Cookies.remove('accessToken')
+      Cookies.remove('refreshToken')
+      window.location.href = routes.login
+      return Promise.reject(toast.error(messages.System.errors.session_expired))
+    }
+  } else {
+    window.location.href = routes.login
+    return Promise.reject(toast.error(messages.System.errors.unauthorized))
+  }
+}
+
+const getErrorMessage = (status: number, data: any) => {
+  switch (status) {
+    case 400:
+      return data?.message || messages.System.errors.bad_request
+    case 403:
+      return messages.System.errors.forbidden
+    case 404:
+      return messages.System.errors.not_found
+    case 409:
+      return data?.message || messages.System.errors.conflict
+    case 422:
+      return data?.message || messages.System.errors.invalid_data
+    case 500:
+    case 501:
+    case 502:
+    case 503:
+      return messages.System.errors.server_error
+    default:
+      return data?.message || messages.System.errors.unknown_error
+  }
+}
+
 http.interceptors.response.use(
   (response) => response.data,
 
   async (error) => {
     if (error.response) {
       const { status, data } = error.response
-      let errorMessage = data?.message
-      switch (status) {
-        case 400:
-          errorMessage = data?.message || messages.System.errors.bad_request
-          break
-        case 401:
-          const refreshToken = Cookies.get('refreshToken')
-          if (refreshToken) {
-            try {
-              const res = await axios.post(
-                `${baseUrl}user/reissue`,
-                { refreshToken },
-                { withCredentials: true },
-              )
 
-              const newAccessToken = res?.data
-              Cookies.set('accessToken', newAccessToken, { expires: 1 / 96 })
-
-              error.config.headers.Authorization = `Bearer ${newAccessToken}`
-              return http.request(error.config)
-            } catch {
-              Cookies.remove('accessToken')
-              Cookies.remove('refreshToken')
-              window.location.href = routes.login
-              return Promise.reject(
-                toast.error(messages.System.errors.session_expired),
-              )
-            }
-          } else {
-            window.location.href = routes.login
-            return Promise.reject(
-              toast.error(messages.System.errors.unauthorized),
-            )
-          }
-        case 403:
-          errorMessage = messages.System.errors.forbidden
-          break
-        case 404:
-          errorMessage = messages.System.errors.not_found
-          break
-        case 409:
-          errorMessage = data?.message || messages.System.errors.conflict
-          break
-        case 422:
-          errorMessage = data?.message || messages.System.errors.invalid_data
-          break
-        case 500:
-        case 501:
-        case 502:
-        case 503:
-          errorMessage = messages.System.errors.server_error
-          break
-        default:
-          errorMessage = data?.message || messages.System.errors.unknown_error
+      if (status === 401) {
+        return handleTokenRefresh(error)
       }
 
+      const errorMessage = getErrorMessage(status, data)
       return Promise.reject(toast.error(errorMessage))
     }
 
